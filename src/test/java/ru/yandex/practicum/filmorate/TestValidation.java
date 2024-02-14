a944e0a4795557a9c4caf8d269c3adab06ac5799
Timur <bear.timur@yandex.ru>
2024-02-14 12:44:58 +0500

    Проект готов к проверке

--- src/test/java/ru/yandex/practicum/filmorate/TestValidation.java
+++ src/test/java/ru/yandex/practicum/filmorate/TestValidation.java
@@ -1,16 +1,11 @@
 package ru.yandex.practicum.filmorate;
 
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
-
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
