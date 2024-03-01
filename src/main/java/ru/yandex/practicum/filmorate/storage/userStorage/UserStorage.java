9e0f811034ece48924c139342be817abb451f084
Timur <bear.timur@yandex.ru>
2024-03-01 10:46:24 +0500

    Проект готов к проверке

--- src/main/java/ru/yandex/practicum/filmorate/storage/userStorage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/userStorage/UserStorage.java
@@ -6,25 +6,15 @@ import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.HashMap;
 
-
 @Component
-
-
 public interface UserStorage {
-
-
     ResponseEntity<?> findAllUsers();
-
 
     ResponseEntity<?> postUser(User user);
 
-
     ResponseEntity<?> updateUser(User user);
-
 
     HashMap<Integer, User> findAll();
 
-
     ResponseEntity<?> getUserById(int id);
-
- }
+}
